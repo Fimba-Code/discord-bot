00aadcb2dddd48c33422743f44a1b2af750d4b04
Faustino Kialungila <faustino.kialungila@gmail.com>
2020-05-20 15:21:40 +0200

    🔧 : fix compilation to commonjs

---
+++ src/bot/__tests__/greet.test.ts
@@ -0,0 +1,11 @@
+describe("Greetings", () => {
+  it("should greet new users", () => {
+    const { greetNewUsers } = require("../functions/greet-new-users")
+    const { FakeDiscordClient } = require("./utils/utils")
+    const client = new FakeDiscordClient()
+    client.on("guildMemberAdd", greetNewUsers)
+    client.fireEvent("guildMemberAdd")
+    expect(client.guild.channels.cache[0].send).toHaveBeenCalledTimes(1)
+    expect(client.guild.channels.cache[0].message).toBeTruthy
+  })
+})
