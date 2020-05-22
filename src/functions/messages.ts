1e06de467d295f373f5b98f33adda73010bfbec9
Faustino Kialungila <faustino.kialungila@gmail.com>
2020-05-22 14:14:39 +0200

    :art: Ran prettier
    Fix code formatting

--- src/functions/messages.ts
+++ src/functions/messages.ts
@@ -7,31 +7,29 @@ import Points from "../constants/points"
 const isCodeBlock = "(`{3})([a-zA-Z0-9_ ])*(`{3})"
 
 const messages = async (message: Message) => {
-  {
-    // ===== GAMIFY ======
-    // URL
-    if (new RegExp(checkUrl).test(message.content)) {
-      await Members.addPoints(
-        `${message.author.username}#${message.author.discriminator}`,
-        Points.linkShare,
-        `${avatarUrl}/${message.author.id}/${message.author.avatar}.${avatarExtension}`
-      )
-    }
-
-    // Code block validator
-    if (new RegExp(isCodeBlock).test(message.content)) {
-      await Members.addPoints(
-        `${message.author.username}#${message.author.discriminator}`,
-        Points.codeBlock,
-        `${avatarUrl}/${message.author.id}/${message.author.avatar}.${avatarExtension}`
-      )
-    }
-    // ===== END GAMIFY ======
-
-    // ===== BOT REPLIES =====
-    if (!message.content.startsWith(prefix) || message.author.bot) return
-    message.reply(replyToUsers(message.content.slice(prefix.length).trim()))
+  // ===== GAMIFY ======
+  // URL
+  if (new RegExp(checkUrl).test(message.content)) {
+    await Members.addPoints(
+      `${message.author.username}#${message.author.discriminator}`,
+      Points.linkShare,
+      `${avatarUrl}/${message.author.id}/${message.author.avatar}.${avatarExtension}`
+    )
   }
+
+  // Code block validator
+  if (new RegExp(isCodeBlock).test(message.content)) {
+    await Members.addPoints(
+      `${message.author.username}#${message.author.discriminator}`,
+      Points.codeBlock,
+      `${avatarUrl}/${message.author.id}/${message.author.avatar}.${avatarExtension}`
+    )
+  }
+  // ===== END GAMIFY ======
+
+  // ===== BOT REPLIES =====
+  if (!message.content.startsWith(prefix) || message.author.bot) return
+  message.reply(replyToUsers(message.content.slice(prefix.length).trim()))
 }
 
 export default messages
