7587a225f054f8634128b3eb67ca2dbcac69b7ed
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2020-06-09 17:11:58 +0200

    new greets message (#25)
    * add: 2 new messages greeting new users
    
    * add: 2 new messages greeting new users
    
    * update: send message only if channel was found
    
    * update: resolve spelling errors
    
    Co-authored-by: git-marcopitra <git-marcopitra@github.com>

--- src/functions/greet-new-users.ts
+++ src/functions/greet-new-users.ts
@@ -2,11 +2,17 @@ import { findChannelsByName, mapToChannelIds } from "../utils/utils"
 
 const greetNewUsers = (member) => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.cache.find(
+  const welcomeChannel = member.guild.channels.cache.find(
     (ch) => ch.name === "welcome"
   )
+  const apresentacoesChannel = member.guild.channels.cache.find(
+    (ch) => ch.name === "apresentações"
+  )
+  const pollChannel = member.guild.channels.cache.find(
+    (ch) => ch.name === "poll"
+  )
   // Do nothing if the channel wasn't found on this server
-  if (!channel) return
+  if (!welcomeChannel) return
   const MODERADORES_ID = "<@&703604014704754708>"
   const [
     general,
@@ -22,7 +28,7 @@ const greetNewUsers = (member) => {
     )
   )
   // Send the message, mentioning the member
-  channel.send(`
+  welcomeChannel.send(`
     Olá <@${member.user.id}>! 
     **Seja muito bem vinda(o) à nossa comunidade!** :rocket:
     > Esse é um espaço para você tirar suas dúvidas, compartilhar suas experiências, e principalmente, evoluir como programador.
@@ -38,6 +44,16 @@ const greetNewUsers = (member) => {
 
     **Happy Hacking**
   `)
+
+  if (apresentacoesChannel) 
+    apresentacoesChannel.send(`
+      <@${member.user.id}>, faça uma breve apresentação e partilhe connosco quem és e o que fazes.
+    `)
+
+  if (pollChannel)
+    pollChannel.send(`
+      <@${member.user.id}>, partilhe connosco as tecnologias que usas, as favoritas e/ou as que queres aprender.
+    `)
 }
 
 export default greetNewUsers
